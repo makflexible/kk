03865de3a679cbefd4dc48467080845b269b0987
thechamp84 <himanshusonkar84@gmail.com>
2023-12-24 17:09:09 +0530

    images fixed

--- src/Utils/ProductList.js
+++ src/Utils/ProductList.js
@@ -192,10 +192,20 @@ const ProductList = [
     description:
       " To cater to a wide range of customers and meet the increasing demand, we have two types of LDPE Pouches, printed and plain. These transparent plain LDPE pouches are waterproof are great for international packing! They come in various sizes with 40micron to 150micron thickness.",
     //  ' " To cater to a wide range of customers and meet the" +
-    //   "increasing demand, we have two types of LDPE Pouches," +
+    //   "increasing demand, we have two types of LDPE Pouches," + 
     //   "printed and plain.",'
   },
 
+  {
+    images: [pro61, pro62, pro63, pro64, pro65, pro65, pro62, pro63, pro64, pro61],
+    heading: "BOPP NON WOVEN BAGS/SACKS",
+    description: `BOPP bags or sacks are the newest type of product branding packaging. They combine PP Woven Fabric, Rotograviour Printing Technology, and BOPP Film Engraved Cylinder. In addition to coming in a variety of densities and sizes, BOPP bags can also be personalised with branding features like logos.
+    BOPP bags are a popular choice in the packaging industry due to its many features :
+    Excellent clarity,
+    Resistance to moisture,
+    Resistance to oil,
+    Superior qualities as a barrier.`
+  },
   // {
   //   images: [pro65, pro62, pro63, pro64, pro61],
   //   heading: "Plain LDPE Pouch",
